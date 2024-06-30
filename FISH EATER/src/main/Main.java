ef112f7df7d7a74db5221d6abbe6aa0ec9db7adb
P4nd@ <160999085+cuongvu513@users.noreply.github.com>
2024-06-30 16:46:30 +0700

    Delete FISH EATER directory

--- FISH EATER/src/main/Main.java
+++
@@ -1,9 +0,0 @@
-package main;
-// Day la chuong trinh chinh 
-public class Main{
-    public static void main(String[] args) {
-        GameMenu gameMenu = new GameMenu();
-        gameMenu.runGame();
-        //new Game();
-    }
-}
